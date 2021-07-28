f13f53e69c3cbdd022993a9b9ed2d73d30d53c07
zF-9 <kewlzewl.kz@gmail.com>
2021-07-28 23:35:19 +0800

    added feature to ignore filename that already in IC format

--- test-py/FileRenamer.py
+++ test-py/FileRenamer.py
@@ -44,85 +44,96 @@ for filename in glob.iglob(root_dir + '**/*' + file_format, recursive=True):
     filter7 = filter6.replace('wl 4G @ ) Profile —', '')
     filter8 = filter7.replace('Cerifcate', '')
     filter9 = filter8.replace('wl 4G @ ) Profile —', '')
-    final = filter9.replace('Vaksinasi', '')
+    filter10 = filter9.replace('‘', '')
+    final = filter10.replace('Vaksinasi', '')
+
+    #filter_chars = ['Digital','Certificate','Sijil','COVID-19','COVID-1 9',
+    #                'wl 4G @ ) Profile —','Cerifcate','Vaksinasi',';', ':',
+    #                '!', "*",'Vaccination']
+    #for j in filter_chars:
+    #    final = result.replace(j, ' ') 
+    
     ########################################################################
     #                        ADD FILTER WORDS DI SINI
     ########################################################################
     
     print(final)
     user = final[0:28]
-    #print(user)
+
     numbers = []
     for word in final.split():
-        if word.isdigit():
+        if word.isdecimal():
             numbers.append(int(word))
             
-    #print(numbers)
+    print(numbers)
     #if numbers is None:
     if not numbers:
         file_name = root_dir + '\\' + user + '.png'
         new_name = user + '.png'
         #print(new_name)
         if path.isfile(new_name):
-            #print("ada duplicate nama")
-            #print("delete lama - save yang baru")
-            print(filename)
-            print(user)
+            print("extracted-output-image: " + user)
+            raw = str(filename)
 
+            # remove characters
+            bad_chars = [';', ':', '!', "*", '.png', 'C:\\Users\\ZF\\Documents\\python\\test-py\\']
+            for i in bad_chars :
+                raw_filter = raw.replace(i, '')
+
+            raw_final_filter = raw_filter.replace('.png', '')
+            print("read_file: " + filename)
+            print("target_file_to: " + file_name)
+
+            # check if same file
             if file_name == filename:
-                print("ini file sama; delete yang latter") # do nothing
-                #os.rename(filename, 'duplicate-' + new_name)
+                print("it's the same file; everthing's fine")
+                #do nothing
             else:
-                print("ini diffrent file")
-                os.rename(filename, 'duplicate-' + new_name)
-                os.remove('duplicate-' + new_name)
-                #os.remove(filename)
-            #file_to_rem = pathlib.Path(new_name)
-            #file_to_rem.unlink()
-            #os.remove(new_name)
-            #os.rename(filename, 'duplicate' + new_name )
+                #if digits != 12; then dia bukan IC
+                print("(Nama): different file")
+                #raw_final_check = int(raw_final_filter)
+                digits = str(len(str(raw_final_filter)))
+                digit = int(digits)
+
+                # check if filename is already IC
+                if raw_final_filter.isdigit() and digit == 12:
+                    print("digit: " + digits)
+                    print("IC la ni: " + str(raw_final_filter))
+                    #do nothing
+                    #skip if file already renamed in IC
+                else:
+                    print("digit: " + digits)
+                    print("bukan IC: " + str(raw_final_filter))
+                    if path.isfile(file_name):
+                        print("file existed!")
+                        os.remove(file_name)
+                        os.rename(filename, new_name)
+                    else:
+                        print("boleh la rename sudah")
+                        os.rename(filename, new_name)
         else:
-            print("ok rename la nama dia")
-            #os.remove() 
+            print("ok rename la nama dia") 
             os.rename(filename, new_name)
     else:
         ic = str(numbers[0])
         file_ic = root_dir + '\\' + ic  + '.png'
         new_ic = ic + '.png'
-        #print(new_ic)
+        print("extracted-output-image: " + ic)
         if path.isfile(new_ic):
-            print(filename)
-            print(file_ic)
+            print("read_file: " + filename)
+            print("target_file: " + file_ic)
 
             if file_ic == filename:
-                print("ini file sama; delete yang latter") #do nothing
-                #os.rename(filename, 'duplicate-' + new_ic)
+                print("it's the same file; everthing's fine")
+                #do nothing
             else:
-                print("ini diffrent file")
+                print("(IC): different files")
                 os.rename(filename, 'duplicate-' + new_ic)
                 os.remove('duplicate-' + new_ic)
-                #os.remove(filename)
-            #print("ada duplicate IC")
-            #print("delete file yang kena tengok")
-            #os.remove(filename)
-            #file_to_rem = pathlib.Path(new_name)
-            #file_to_rem.unlink()
-            #os.remove(new_name)
-            #os.rename(filename, 'duplicate' + new_ic )
+
         else:
-            print("ok rename la ic dia")
+            print("ok rename la IC dia")
             os.rename(filename, new_ic)
 
-    #orig_stdout = sys.stdout
-    #f = open('outpur.txt', 'w')
-    #sys.stdout = f
+    print("###############################################################################")
 
-    #for i in range(2):
-    #    print('i= ', i)
-
-    #sys.stdout = orig_stdout
-    #f.close()
-
-#file = open('output.txt', 'rt')
-#contents = file.read() 
-#print(contents)
