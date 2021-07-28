eabe9c084548a2207006ab5f5d2c195af9c35c10
zF-9 <kewlzewl.kz@gmail.com>
2021-07-28 14:40:13 +0800

    added filtering for duplicated images automatically; remove if image already exist; prevent more than one file being rename the same"

--- test-py/FileRenamer.py
+++ test-py/FileRenamer.py
@@ -8,17 +8,21 @@ import subprocess
 from io import StringIO
 import linecache
 from subprocess import Popen, PIPE
+import os.path
+from os import path
 
 pytesseract.pytesseract.tesseract_cmd = r'C:\Program Files\Tesseract-OCR\tesseract'
 root_dir = r'C:\Users\ZF\Documents\python\test-py'
 
+file_format = ".png" #change format globally , eg: .png/.jpeg/.jpg
+
 
 # .FILE FORMAT CHANGEABLE
-for filename in glob.iglob(root_dir + '**/*.jpg', recursive=True):
+for filename in glob.iglob(root_dir + '**/*' + file_format, recursive=True):
     pattern = r"\Digital Certificate\b" or r"\Sijil Digital\b"
     keyword = {'Digital','Certificate', 'Sijil','COVID-19', 'Vaccination', 'Vaksinasi' }
-    #image = r'C:\Users\ZF\Pictures\Sabah Elite percipio - Copy.png'
-    #image = r'C:\Users\ZF\Documents\python\test-py\6.png'
+    #image = r'C:\Users\ZF\Pictures\Sabah Elite percipio - Copy.png' - test image
+    #image = r'C:\Users\ZF\Documents\python\test-py\6.png' - test image
     textImg = pytesseract.image_to_string(filename)
     text = str(textImg.strip())
     queryword = text.split()
@@ -27,7 +31,10 @@ for filename in glob.iglob(root_dir + '**/*.jpg', recursive=True):
     
     resultword = [word for word in queryword if word.lower() not in keyword]
     result = ' '.join(resultword)
+    
     ########################################################################
+    #                        ADD FILTER WORDS DI SINI
+    ########################################################################    
     filter1 = result.replace('Digital', '')
     filter2 = filter1.replace('Certificate', '')
     filter3 = filter2.replace('Vaccination', '')
@@ -35,13 +42,15 @@ for filename in glob.iglob(root_dir + '**/*.jpg', recursive=True):
     filter5 = filter4.replace('COVID-19', '')
     filter6 = filter5.replace('COVID-1 9', '')
     filter7 = filter6.replace('wl 4G @ ) Profile —', '')
-    filter7 = filter6.replace('Cerifcate', '')
-    final = filter7.replace('Vaksinasi', '')
+    filter8 = filter7.replace('Cerifcate', '')
+    filter9 = filter8.replace('wl 4G @ ) Profile —', '')
+    final = filter9.replace('Vaksinasi', '')
     ########################################################################
     #                        ADD FILTER WORDS DI SINI
     ########################################################################
+    
     print(final)
-    user = final[0:23]
+    user = final[0:28]
     #print(user)
     numbers = []
     for word in final.split():
@@ -51,46 +60,58 @@ for filename in glob.iglob(root_dir + '**/*.jpg', recursive=True):
     #print(numbers)
     #if numbers is None:
     if not numbers:
-        print(user)
-        os.rename(filename, user + '.png')
+        file_name = root_dir + '\\' + user + '.png'
+        new_name = user + '.png'
+        #print(new_name)
+        if path.isfile(new_name):
+            #print("ada duplicate nama")
+            #print("delete lama - save yang baru")
+            print(filename)
+            print(user)
+
+            if file_name == filename:
+                print("ini file sama; delete yang latter") # do nothing
+                #os.rename(filename, 'duplicate-' + new_name)
+            else:
+                print("ini diffrent file")
+                os.rename(filename, 'duplicate-' + new_name)
+                os.remove('duplicate-' + new_name)
+                #os.remove(filename)
+            #file_to_rem = pathlib.Path(new_name)
+            #file_to_rem.unlink()
+            #os.remove(new_name)
+            #os.rename(filename, 'duplicate' + new_name )
+        else:
+            print("ok rename la nama dia")
+            #os.remove() 
+            os.rename(filename, new_name)
     else:
-        ic = str(numbers[0])       
-        print(ic)
-        os.rename(filename, ic + '.png')
+        ic = str(numbers[0])
+        file_ic = root_dir + '\\' + ic  + '.png'
+        new_ic = ic + '.png'
+        #print(new_ic)
+        if path.isfile(new_ic):
+            print(filename)
+            print(file_ic)
 
-    #print(substring[55:70])
-    #sys.stdout = open(root_dir + '_output_tested.txt', 'a+')
-    #keyword = 'Digital Certificate'
-    #stripped = text.split(keyword)
-    #print(stripped) 
-    #before_keyword, keyword, after_keyword = mystring.partition(pattern)
-    #user = re.search('(?<=Digital Certificate)(\w+)',text)
-    #name = user.group()
-    #name = re.findall(pattern, text)
-    #username = text.split("Digital Certificate", 1)
-    #user = re.match()
-    #for element in keyword:
-    #while 'Digital Certificate' not in text.readline():
-    #    continue
-    #print(f.readline())
-    #name = text.split("Digital Certificate",3)[1]
-    #print(user)
-    #iterator = iter(lines.splitlines())
-    #content = textImg.readlines()
-    #print(text[0:3])
-    #print(text[39:65])
-    #print(linecache.getline('test-py_outputs.txt',4))
-    
-    
-    #proc=subprocess.Popen('Echo to stdout', shell=True, stdout=subprocess.PIPE, )
-    #output = proc.communicate()
-    #print(output)
-    #f = open(root_dir + '_outputed.txt', 'a+')
-    #sys.stdout = f
-    #f.close()
-    
-
-#os.rename(image,)
+            if file_ic == filename:
+                print("ini file sama; delete yang latter") #do nothing
+                #os.rename(filename, 'duplicate-' + new_ic)
+            else:
+                print("ini diffrent file")
+                os.rename(filename, 'duplicate-' + new_ic)
+                os.remove('duplicate-' + new_ic)
+                #os.remove(filename)
+            #print("ada duplicate IC")
+            #print("delete file yang kena tengok")
+            #os.remove(filename)
+            #file_to_rem = pathlib.Path(new_name)
+            #file_to_rem.unlink()
+            #os.remove(new_name)
+            #os.rename(filename, 'duplicate' + new_ic )
+        else:
+            print("ok rename la ic dia")
+            os.rename(filename, new_ic)
 
     #orig_stdout = sys.stdout
     #f = open('outpur.txt', 'w')
